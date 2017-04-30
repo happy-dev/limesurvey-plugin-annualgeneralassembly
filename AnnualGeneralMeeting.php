f5ee1e3bcdf6d9dbfb6398a4dea1b9a691904c4a
Alexandre Bourlier <alexandre@happy-dev.fr>
2017-04-30 10:20:28 +0200

    [bugfix] name & description

--- AnnualGeneralMeeting.php
+++ AnnualGeneralMeeting.php
@@ -6,8 +6,8 @@ use \ls\menu\MenuItem;
  * Interface with LimeSurvey's API
  */
 class AnnualGeneralMeeting extends PluginBase {
-  static protected $name        = '';
-  static protected $description = '';
+  static protected $name        = 'Annual General Meeting';
+  static protected $description = 'Voting at Annual Assembly Meetings made easy';
   protected $storage            = 'DbStorage';
   protected $defaultSettings    = '{"Consommateurs": 0.2, "Salariés": 0.2, "Producteurs": 0.6}';
   
@@ -15,28 +15,9 @@ class AnnualGeneralMeeting extends PluginBase {
   public function __construct(PluginManager $manager, $id) {
     parent::__construct($manager, $id);
 
-    self::$name         = $this->getProperty('name');
-    self::$description  = $this->getProperty('description');
-
     $this->subscribe('beforeSurveySettings');
     $this->subscribe('newSurveySettings');
     $this->subscribe('beforeToolsMenuRender');
-  }
-
-
-  public function getProperty($key) {
-    switch($key) {
-      case 'name':
-        return gT("Annual General Meeting");
-        break;
-
-      case 'description':
-        return gT("Voting at Annual Assembly Meetings made easy");
-        break;
-
-      default:
-        return $key . ' is not a valid key';
-    }
   }
 
 
